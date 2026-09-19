997d9280fbd71fa8275afb3a84786cfec0fed178
Ernesto Serrano <info@ernesto.es>
2026-09-19 23:01:52 +0100

    style: normalize strict types file headers

--- src/ParserContext.php
+++ src/ParserContext.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * Shared mutable parser state for nested MIME parsing.
  *
@@ -11,6 +9,9 @@ declare(strict_types=1);
  * @license  MIT https://opensource.org/licenses/MIT
  * @link     https://github.com/erseco/mime-mail-parser
  */
+
+declare(strict_types=1);
+
 namespace Erseco;
 
 /**
